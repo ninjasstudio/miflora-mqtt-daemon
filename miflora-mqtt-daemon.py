3b79be5c2775a36406742c0ef043cf9546ca72a3
ninjasstudio <21231061+ninjasstudio@users.noreply.github.com>
2021-07-29 00:31:41 -0400

    changed qos for ha
    changed qos for ha

--- miflora-mqtt-daemon.py
+++ miflora-mqtt-daemon.py
@@ -346,7 +346,7 @@ elif reporting_mode == 'homeassistant-mqtt':
                     'sw_version': flora['firmware']
             }
             payload['force_update'] = True
-            payload['expire_after'] = '600'
+            payload['expire_after'] = '60'
             mqtt_client.publish(discovery_topic, json.dumps(payload), 1, True)
 elif reporting_mode == 'gladys-mqtt':
     print_line('Announcing Mi Flora devices to MQTT broker for auto-discovery ...')
@@ -437,7 +437,7 @@ while True:
             sleep(0.5) # some slack for the publish roundtrip and callback function
         elif reporting_mode == 'homeassistant-mqtt':
             print_line('Publishing to MQTT topic "{}/sensor/{}/state"'.format(base_topic, flora_name.lower()))
-            mqtt_client.publish('{}/sensor/{}/state'.format(base_topic, flora_name.lower()), json.dumps(data))
+            mqtt_client.publish('{}/sensor/{}/state'.format(base_topic, flora_name.lower()), json.dumps(data), 1, retain=True)
             sleep(0.5) # some slack for the publish roundtrip and callback function
         elif reporting_mode == 'gladys-mqtt':
             print_line('Publishing to MQTT topic "{}/mqtt:miflora:{}/feature"'.format(base_topic, flora_name.lower()))
